96eda5977ebd5908b07b482eab99a41840c756ab
ErickArtola <egeovanyartola@gmail.com>
2023-05-09 00:37:41 -0400

    allocation and currency

--- src/components/AllocationForm.js
+++ src/components/AllocationForm.js
@@ -59,7 +59,9 @@ const AllocationForm = (props) => {
                         <option defaultValue value="Add" name="Add">Add</option>
                 <option value="Reduce" name="Reduce">Reduce</option>
                   </select>
-
+                    
+                    
+                    <span style={{ "position":"relative","right":"-20px"}}> £ </span>   
                     <input
                         required='required'
                         type='number'
--- src/components/Currency.js
+++ src/components/Currency.js
@@ -2,7 +2,7 @@ import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Currency = () => {
-  const {dispatch } = useContext(AppContext);
+  const {dispatch} = useContext(AppContext);
 
 	const changeCurrency = (val)=>{
 			dispatch({
@@ -17,9 +17,9 @@ const Currency = () => {
 	
    return (
 		<div className='alert alert-secondary'>
-        <label style={{marginLeft: '1rem' , backgroundColor:'#93e499', color:'white'}} >Currency
-      <select name='hover_color'id="currency" onChange={(event)=>changeCurrency(event.target.value)} style={{ marginLeft: '1rem' , backgroundColor:'#93e499', color:'white'}}>
-        <option style={{color:'black'}} value="£">£ Pound</option>
+        <label style={{marginLeft: '1rem' , backgroundColor:'#93e499', color:'white'}} > Currency
+        <select name='hover_color' id="currency" onChange={(event)=>changeCurrency(event.target.value)} style={{ marginLeft: '1rem' , backgroundColor:'#93e499', color:'white'}}>
+        <option style={{color:'black'}} value="£"> (£ Pound) </option>
         <option style={{color:'black'}} value="$">$ Dollar</option>
         <option style={{color:'black'}} value="€">€ Euro</option>
         <option style={{color:'black'}} value="₹">₹ Rupee</option>
