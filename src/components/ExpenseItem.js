03e23a3607389e0f4e3574ebe8259f6e616f053d
ErickArtola <egeovanyartola@gmail.com>
2023-05-10 07:44:37 -0400

    updated expenseitem and expenselist components for task 6

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -25,11 +25,26 @@ const ExpenseItem = (props) => {
 
     }
 
+    const decreaseAllocation = (name) => {
+        const expense = {
+            name: name,
+            cost: 10,
+        };
+
+        dispatch({
+            type: 'SUBTRACT_EXPENSE',
+            payload: expense
+        });
+
+    }
+
     return (
         <tr>
         <td>{props.name}</td>
         <td>{currency}{props.cost}</td>  
         <td><button onClick={event=> increaseAllocation(props.name)}>+</button></td>
+        <td><img src="https://pngimg.com/uploads/plus/plus_PNG121.png" alt="-" width="48" height="48" onClick={event=> increaseAllocation(props.name)}></img></td>
+        <td><img src="https://pngimg.com/uploads/minus/minus_PNG60.png" alt="-" width="48" height="48" onClick={event=> decreaseAllocation(props.dname)}></img></td>
         <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
         </tr>
     );
